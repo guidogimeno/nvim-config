f0c955908d73345dc13182704a81933b3433df4f
Guido Gimeno <gimenoguido@gmail.com>
2023-10-02 00:50:25 -0300

    update

--- lua/ggimeno/remap.lua
+++ lua/ggimeno/remap.lua
@@ -6,6 +6,8 @@ vim.keymap.set("i", "jj", "<Esc>", { noremap = true })
 vim.keymap.set("n", "<leader>v", ":vsplit<CR><C-w>l", { noremap = true })
 vim.keymap.set("n", "<leader>h", ":wincmd h<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>l", ":wincmd l<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>j", ":wincmd j<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>k", ":wincmd k<CR>", { noremap = true })
 
 -- copy and paste from clipboard
 vim.keymap.set("v", "<leader>y", "\"+y")
