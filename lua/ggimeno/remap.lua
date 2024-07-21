665067bda550ea3279b1ee083a2bf43b8fe379f0
guidogimeno <gimenoguido@gmail.com>
2024-07-21 19:05:15 -0300

    feat: update C-d -u

--- lua/ggimeno/remap.lua
+++ lua/ggimeno/remap.lua
@@ -19,8 +19,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- scroll
-vim.keymap.set("n", "}", "<C-d>zz")
-vim.keymap.set("n", "{", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- keep search terms in the middle
 vim.keymap.set("n", "n", "nzzzv")
