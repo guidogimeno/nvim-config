3ff704eaeb5fe991b2fa1aa5ca1746a6048b457b
guidogimeno <gimenoguido@gmail.com>
2025-01-01 19:14:50 -0300

    feat: moon

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,8 @@
 require("rose-pine").setup({
+    variant = "moon",
+    dark_variant = "moon",
     disable_background = true,
     disable_italic = true,
 });
 
 vim.cmd("colorscheme rose-pine")
-
